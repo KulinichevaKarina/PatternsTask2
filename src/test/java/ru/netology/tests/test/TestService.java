9badf5e5307c9c3311ed752667a8cb2324055d2d
Karina <kulinicheva_91@mail.ru>
2023-09-12 01:08:13 +0300

    code correction 2

--- src/test/java/ru/netology/tests/test/TestService.java
+++ src/test/java/ru/netology/tests/test/TestService.java
@@ -25,7 +25,7 @@ class TestService {
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".App_appContainer__3jRx1")
+        $("h2")
                 .shouldHave(Condition.exactText("  Личный кабинет"))
                 .shouldBe(Condition.visible);
     }
@@ -37,7 +37,7 @@ class TestService {
         $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
         $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".notification__content")
+        $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
                 .shouldBe(Condition.visible);
     }
@@ -49,9 +49,10 @@ class TestService {
         $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
         $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".notification__content")
+        $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Ошибка! Пользователь заблокирован"))
                 .shouldBe(Condition.visible);
+        Thread.sleep(5000);
     }
 
     @Test
@@ -62,7 +63,7 @@ class TestService {
         $("[data-test-id='login'] input").setValue(wrongLogin);
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(".notification__content")
+        $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
                 .shouldBe(Condition.visible);
     }
@@ -75,7 +76,7 @@ class TestService {
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(wrongPassword);
         $("[data-test-id='action-login']").click();
-        $(".notification__content")
+        $("[data-test-id='error-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
                 .shouldBe(Condition.visible);
 
